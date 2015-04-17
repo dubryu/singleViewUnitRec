d70c8403573da6075b6b5239ca0466c3265bb821
dubryu <katayamaryuusuke@katayama-no-MacBook-Pro.local>
2015-04-17 19:35:28 +0900

    beforeFixed

---
+++ RecThroughUnit/setRotation.h
@@ -0,0 +1,13 @@
+//
+//  setRotation.h
+//  RecThroughUnit
+//
+//  Created by KatayamaRyusuke on 2015/04/17.
+//  Copyright (c) 2015年 片山隆介. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface setRotation : UINavigationController
+
+@end
